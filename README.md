08dedcc1efed973487bb763b14e39f4eef10d917
jiangzhe <zhe.jiang@transwarp.io>
2018-09-06 20:02:07 +0800

    Add http tunnelling

--- README.md
+++ README.md
@@ -1,6 +1,74 @@
 # http-examples
 
+## Introduction
+
+- 超文本传输协议
+- 请求/应答模式
+- 统一资源标识符(URI)
+- 无状态
+- 元数据支持
+- 分布式
+- 多媒体
+
+## Request
+
+![http-request](static/http-request.jpg)
+
+## Response
+
+![http-response](static/http-response.jpg)
+
+## Method
+
+- GET 查询指定资源
+- POST 创建新资源
+- HEAD 查询指定资源（仅返回元数据）
+- PUT 
+- DELETE
+- OPTIONS
+- TRACE 
+- PATCH 资源的部分更新
+- CONNECT 变更为TCP/IP
+
+## Status
+
+- 101 更换协议（例如建立websocket时）
+- 200 成功
+- 201 成功，资源被创建
+- 301 永久重定向
+- 302 临时重定向
+- 304 资源未被修改（与Last-Modified或Etag联合使用）
+- 307 临时重定向（复制原请求，HTTP/1.1）
+- 400 错误请求
+- 401 未认证
+- 403 无权限
+- 404 资源不存在
+- 500 服务器内部错误
+- ...
+
+[wikipedia详细说明](https://en.wikipedia.org/wiki/List_of_HTTP_status_codes)
+
+## Header
+
+- Host
+- Connection
+- Transfer-Encoding
+- Content-Type
+- Content-Length
+- 
+
+## Cookie
 
 
----
+## Connection
+
+
+## Cache
+
+## Proxy
+
+- 反向代理
+- HTTP隧道
+
+![http-tunnel](static/http-tunnel.png)
 
--- server/server.go
+++ server/server.go
@@ -7,16 +7,39 @@ import (
 	"io/ioutil"
 	"time"
 	"math/rand"
+	"strings"
+	"os"
+	"crypto/md5"
+	"io"
+	"net"
+	"sync"
+	"crypto/tls"
 )
 
+const staticDir = "/home/jiangzhe/go/src/http-examples/static"
+
 func main() {
+	// for debug
 	rand.Seed(1)
 
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("/home/jiangzhe/go/src/http-examples/static"))))
-	http.HandleFunc("/requests", Requests)
-	http.HandleFunc("/chunks", Chunks)
-	err := http.ListenAndServe(":8080", nil)
-	log.Fatal(err)
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(staticDir))))
+	serveMux.HandleFunc("/etags/", Etags)
+	serveMux.HandleFunc("/requests", Requests)
+	serveMux.HandleFunc("/chunks", Chunks)
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodConnect {
+				Proxy(w, r)
+			} else {
+				serveMux.ServeHTTP(w, r)
+			}
+		}),
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+	log.Fatal(server.ListenAndServe())
 }
 
 // function to show request information
@@ -30,6 +53,7 @@ func Requests(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Header {
 		writeLine(w, fmt.Sprintf("%v: %v", k, v))
 	}
+
 
 	if err := r.ParseForm(); err == nil {
 		if len(r.Form) > 0 {
@@ -47,8 +71,7 @@ func Requests(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Body != nil {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
+		if body, err := ioutil.ReadAll(r.Body); err == nil {
 			writeLine(w, "-- body")
 			w.Write(body)
 		}
@@ -76,6 +99,113 @@ func Chunks(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusInternalServerError)
 }
 
+func Etags(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, "/etags/") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	filePath := staticDir + "/" + strings.TrimPrefix(r.URL.Path, "/etags/")
+	file, err := os.Open(filePath)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+	buf := make([]byte, 4096)
+	n := 0
+	hash := md5.New()
+	for {
+		n, err = file.Read(buf)
+		if err != nil {
+			break
+		}
+		if n == 0 {
+			continue
+		}
+		rb := buf[:n]
+		hash.Write(rb)
+	}
+	if err != nil && err != io.EOF {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fileEtag := fmt.Sprintf("\"%x\"", hash.Sum(nil))
+	etag := r.Header.Get("If-None-Match")
+	if len(etag) != 0 && etag == fileEtag {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+	w.Header().Set("Etag", fileEtag)
+	writeFile(w, filePath)
+}
+
+func Proxy(w http.ResponseWriter, r *http.Request) {
+
+	h, ok := w.(http.Hijacker)
+	if !ok {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	netConn, brw, err := h.Hijack()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if brw.Reader.Buffered() > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	proxyConn, err := net.Dial("tcp", r.URL.Host)
+	if err != nil {
+		// write error response to hijacked connection
+		netConn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+		netConn.Close()
+		return
+	}
+
+	// disable original deadline
+	netConn.SetDeadline(time.Time{})
+
+	var pipeClosure sync.Once
+
+	var pipe = func(src, dst net.Conn) {
+		defer pipeClosure.Do(func() {
+			src.Close()
+			dst.Close()
+		})
+
+		buf := make([]byte, 8192)
+		for {
+			src.SetReadDeadline(time.Now().Add(10 * time.Second))
+			n, err := src.Read(buf)
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				continue
+			}
+			if err != nil {
+				return
+			}
+			b := buf[:n]
+			_, err = dst.Write(b)
+			if err != nil {
+				return
+			}
+		}
+	}
+	go pipe(netConn, proxyConn)
+	go pipe(proxyConn, netConn)
+
+	_, err = netConn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+	if err != nil {
+		log.Println(err)
+		pipeClosure.Do(func() {
+			netConn.Close()
+			proxyConn.Close()
+		})
+	}
+}
+
 // helper functions
 
 var newline = []byte("\n")
@@ -85,3 +215,12 @@ func writeLine(w http.ResponseWriter, s string) {
 	w.Write(newline)
 }
 
+func writeFile(w http.ResponseWriter, filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+	io.Copy(w, file)
+}
